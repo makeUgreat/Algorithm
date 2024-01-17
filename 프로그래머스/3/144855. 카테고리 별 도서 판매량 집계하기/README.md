03168f8e64f33759953c2c678a120153e076c894
makeUgreat <137480733+makeUgreat@users.noreply.github.com>
2024-01-18 08:22:13 +0900

    [level 3] Title: 카테고리 별 도서 판매량 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/README.md
+++ 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/README.md
@@ -1,6 +1,6 @@
 # [level 3] 카테고리 별 도서 판매량 집계하기 - 144855 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/144855) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/144855#qna) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 1월 4일 8:9:56
+2024년 1월 4일 8:22:11
 
 ### 문제 설명
 
--- 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
+++ 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
@@ -1,12 +1,9 @@
 SELECT
     A.category,
-    SUM(B.sales) AS total_sales
-FROM book as A
-INNER JOIN book_sales as B
+    SUM(B.sales) AS totl_sales
+FROM book A INNER JOIN book_sales B
 ON A.book_id = B.book_id
 WHERE
-    DATE_FORMAT(B.sales_date,'%Y-%m') = '2022-01'
-GROUP BY category
-ORDER BY category
-
-    
+    TO_CHAR(B.sales_date,'YYYY-MM') = '2022-01'
+GROUP BY A.category
+ORDER BY A.category
