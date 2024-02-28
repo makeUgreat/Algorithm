3de78515202e96c3079916f242375918c5af8126
makeUgreat <137480733+makeUgreat@users.noreply.github.com>
2024-02-28 14:23:48 +0900

    [level 1] Title: 평균 구하기, Time: 3.07 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    public double solution(int[] arr) {
+        double answer = Arrays.stream(arr)
+                            .average()
+                            .orElse(Double.NaN);
+        
+        return answer;
+    }
+}
