25f562a2fa904d1e43c5986d3e3c33ae1727af97
makeUgreat <137480733+makeUgreat@users.noreply.github.com>
2024-03-03 22:19:36 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,11 +1,2 @@
 def solution(n):
-    cost = 0
-    while n >= 1:
-    
-        if n % 2 == 0:
-            n //= 2
-        else:
-            n -= 1
-            cost += 1
-        
-    return cost
+        return bin(n).count('1')
