8f7c49de138a9d80e3d7e69086948f9a5e7b2d94
makeUgreat <137480733+makeUgreat@users.noreply.github.com>
2024-03-11 23:15:04 +0900

    [level 3] Title: 대여 기록이 존재하는 자동차 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/157341. 대여 기록이 존재하는 자동차 리스트 구하기/README.md
+++ 프로그래머스/3/157341. 대여 기록이 존재하는 자동차 리스트 구하기/README.md
@@ -1,6 +1,6 @@
 # [level 3] 대여 기록이 존재하는 자동차 리스트 구하기 - 157341 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/157341) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/157341?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 03월 11일 23:06:55
+2024년 03월 11일 23:15:03
 
 ### 문제 설명
 
--- 프로그래머스/3/157341. 대여 기록이 존재하는 자동차 리스트 구하기/대여 기록이 존재하는 자동차 리스트 구하기.sql
+++ 프로그래머스/3/157341. 대여 기록이 존재하는 자동차 리스트 구하기/대여 기록이 존재하는 자동차 리스트 구하기.sql
@@ -1,14 +1,14 @@
 select
     distinct(car.car_id)
 from
-    car_rental_company_car as car
+    car_rental_company_car car
     inner join
-    car_rental_company_rental_history as history
+    car_rental_company_rental_history history
     on car.car_id = history.car_id
 where
-    date_format(history.start_date,'%m') = '10'
+    to_char(history.start_date,'mm') = '10'
     and
-    car_type = '세단'
+    car.car_type = '세단'
 order by
     car.car_id desc;
     
