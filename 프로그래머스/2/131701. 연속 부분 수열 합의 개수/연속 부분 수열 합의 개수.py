7d11e8dbff2f78bfddf17323eeb1360203274339
makeUgreat <137480733+makeUgreat@users.noreply.github.com>
2024-03-23 11:55:42 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 676.84 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,18 +1,36 @@
 def solution(elements):
+    # 누적합으로 최적화해보자
+    prefixSum = [0]
     
-    circle = elements * 2
-    print(circle) 
-    cnt = 0
-    dupl = set()
-    for l in range(1,len(elements)+1):
-        sum_ = 0
-        for k in range( len(circle) - l):
-            now = sum(circle[k:k+l])
-            if now not in dupl:
-                # print(l, circle[k:k+l])
-                dupl.add(now)
-                cnt += 1
+    for e in elements:
+        prefixSum.append(prefixSum[-1]+e)
+        
+    only = set()
+    for start in range(len(elements)):
+        for length in range(1,len(elements)+1):
+            end = start + length - 1
+            
+            sum_ = prefixSum[end%len(elements)] - prefixSum[start] + (prefixSum[-1] if end >= len(elements) else 0 ) 
+            only.add(sum_)
             
     
-    answer = cnt
-    return answer
+    return len(only)
+    
+    
+    # 기존풀이
+    
+#     circle = elements * 2
+#     cnt = 0
+#     dupl = set()
+#     for l in range(1,len(elements)+1):
+#         sum_ = 0
+#         for k in range( len(circle) - l):
+#             now = sum(circle[k:k+l])
+#             if now not in dupl:
+#                 # print(l, circle[k:k+l])
+#                 dupl.add(now)
+#                 cnt += 1
+            
+    
+#     answer = cnt
+#     return answer
