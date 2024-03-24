1776a6106130203b17d48c56f35cbcfbff83f236
makeUgreat <137480733+makeUgreat@users.noreply.github.com>
2024-03-24 11:41:08 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/README.md
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/README.md
@@ -1,6 +1,6 @@
 # [level 3] 즐겨찾기가 가장 많은 식당 정보 출력하기 - 131123 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131123) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131123?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 03월 24일 11:29:31
+2024년 03월 24일 11:45:16
 
 ### 문제 설명
 
--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -6,10 +6,9 @@ select
 from rest_info a inner join (
     select
         food_type,
-        max(favorites) as favorites
+        max(favorites) as mx_fav
     from rest_info
-    group by food_type
-    ) b 
-    on a.favorites = b.favorites and a.food_type = b.food_type
+    group by food_type 
+) b on a.food_type = b.food_type and a.favorites = b.mx_fav
 order by
     food_type desc;
