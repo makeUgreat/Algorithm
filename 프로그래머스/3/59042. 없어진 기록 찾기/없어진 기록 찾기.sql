5a8f2718b5d0f0e96ee0999ce4df0861867a2d65
makeUgreat <137480733+makeUgreat@users.noreply.github.com>
2024-03-26 21:20:27 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,10 +1,13 @@
+# animal_outs에는 있는데 animal_ins 는 없는 테이블 찾기
+
 select
     outs.animal_id,
     outs.name
 from
-    animal_outs as outs left join animal_ins as ins
+    animal_outs outs left join animal_ins ins
     on outs.animal_id = ins.animal_id
 where
-    ins.datetime is null
+    ins.animal_id is null
 order by
-    outs.animal_id, outs.name;
+    ins.animal_id,
+    ins.name
