2024e087619339a25d8b9399fcf5e4205c8ba928
makeUgreat <137480733+makeUgreat@users.noreply.github.com>
2024-04-03 20:10:06 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/README.md
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/README.md
@@ -1,6 +1,6 @@
 # [level 3] 조건에 맞는 사용자 정보 조회하기 - 164670 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/164670) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/164670#qna) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 04월 03일 19:54:34
+2024년 04월 03일 20:14:23
 
 ### 문제 설명
 
--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,12 +1,16 @@
-
+-- 코드를 입력하세요
 SELECT
-    user.user_id,
+    user_id,
     nickname,
-    concat(city,' ',street_address1,' ',street_address2) as 전체주소,
-    concat(substr(tlno,1,3),'-',substr(tlno,4,4),'-',substr(tlno,8,4)) as 전화번호
+    city || ' ' || street_address1 || ' ' || street_address2 as 전체주소,
+    substr(B.TLNO, 1,3) || '-' || substr(B.TLNO, 4,4) || '-' || substr(B.TLNO, 8,4) 전화번호
 from
-    used_goods_board board inner join used_goods_user user
-    on board.writer_id = user.user_id
-group by user_id
-having count(user_id) >= 3
-order by user.user_id desc
+    used_goods_user b
+where
+    b.user_id in (
+    select writer_id
+    from used_goods_board
+    group by writer_id
+    having count(1) >= 3)
+order by
+    user_id desc
