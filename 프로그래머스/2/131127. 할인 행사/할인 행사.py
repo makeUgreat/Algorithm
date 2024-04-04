444679038c35c5f8864d75982bfdab50fcfbdcfe
makeUgreat <137480733+makeUgreat@users.noreply.github.com>
2024-04-04 20:28:02 +0900

    [level 2] Title: 할인 행사, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -3,19 +3,11 @@ def stock_check(want_stock):
         if v != 0:
             return True
     return False
-    
-
-        
-    
 
 def solution(want, number, discount):  
     # discount 배열에서 want 배열 길이 만큼 슬라이싱 윈도우해서
     # 다 있으면 해당 슬라이스 배열의 인덱스 번호를 리턴한다
     lng = len(want)
-    stock = dict()
-    for k in discount:
-        stock[k] = stock.get(k,0) + 1
-    
     want_stock = dict()
     # 품목별 재고 딕셔너리로 만들기
     for k,v in zip(want,number):
@@ -34,8 +26,7 @@ def solution(want, number, discount):
                 want_stock[item] -= 1
         
         if stock_check(want_stock): continue
-        
+    
         answer += 1
         
-                
     return answer
