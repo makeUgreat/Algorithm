79c38b546dd3c96f7ae7148c556a8a022b57ac7f
makeUgreat <137480733+makeUgreat@users.noreply.github.com>
2024-04-04 15:30:46 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,17 @@
+SELECT
+    board_id,
+    writer_id,
+    title,
+    price,
+    case 
+        when status = 'SALE' then '판매중'
+        when status = 'RESERVED' then '예약중'
+        when status = 'DONE' then '거래완료'
+        else status
+    end as status
+from
+    used_goods_board
+where
+    to_char(created_date,'yyyy-mm-dd') = '2022-10-05'
+order by
+    board_id desc;
