1ac9d4014495b29d67b80d21fad976d04c98d5d1
makeUgreat <137480733+makeUgreat@users.noreply.github.com>
2024-04-07 21:21:04 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/README.md
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/README.md
@@ -1,6 +1,6 @@
 # [level 4] 서울에 위치한 식당 목록 출력하기 - 131118 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131118) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131118?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 04월 07일 21:16:37
+2024년 04월 07일 21:21:03
 
 ### 문제 설명
 
--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -1,18 +1,23 @@
-select
+-- 코드를 입력하세요
+SELECT
     i.rest_id,
     i.rest_name,
     i.food_type,
     i.favorites,
     i.address,
     round(avg(r.review_score),2) as score
-    
-from
-    rest_info i
-    inner join rest_review r on r.rest_id = i.rest_id
+
+from rest_info i
+    inner join rest_review r on i.rest_id = r.rest_id
+
 where
     i.address like '서울%'
 group by
-    i.rest_id
+    i.rest_id,
+    i.rest_name,
+    i.food_type,
+    i.favorites,
+    i.address
 order by
-    score desc, i.favorites desc;
+    round(avg(r.review_score),2) desc, favorites desc;
     
