4e070c4678005f613f67be21ac673142fd340159
makeUgreat <137480733+makeUgreat@users.noreply.github.com>
2024-04-10 22:24:35 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 224 ms, Memory: 49208 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -1,9 +1,12 @@
+import sys
+input = sys.stdin.readline
+
 n,m = map(int,input().split())
 table = dict()
 
 for _ in range(n):
-    url,pwd = input().split(' ')
+    url,pwd = input().strip().split(' ')
     table[url] = pwd
 
 for _ in range(m):
-    print(table[input()])
+    print(table[input().strip()])
